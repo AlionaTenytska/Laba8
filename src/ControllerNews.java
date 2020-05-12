3caef95075e2eeb6dbfa6193229171b0d88e9b3a
tenickajaalena <tenickajaalena@gmail.com>
2020-05-12 22:21:51 +0300

    Initial commit

---
+++ src/ControllerNews.java
@@ -0,0 +1,17 @@
+public class ControllerNews {
+
+    private News [] arrayNews;
+
+    ControllerNews(News [] arrayNews){
+        this.arrayNews=arrayNews;
+    }
+
+    public void view(int id){
+        System.out.println("Новость с id = "+ id + arrayNews[id].newsInform());
+    }
+
+    public void edit(int id, String title){
+        System.out.println("Новость с id = "+ id +arrayNews[id].changeNews(title));
+    }
+
+}
